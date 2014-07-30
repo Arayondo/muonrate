fde4d94002493b083a7512e97263cfe03dbb244a
ctheiss <christophe.theiss@student.kit.edu>
2014-07-30 16:01:46 +0200

    Improves doppelpulssuche.py and plot_threshold.py scripts

--- doppelpulssuche.py
+++ doppelpulssuche.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+# Script to search for double pulses from muon decay
+# Algorithm for line analysis:
+# 1. Check if line is a valid DAQ-Word
+# 2. Check if a new event has been triggered
+#	2.y.1. increment eventscounter
+#	2.y.2. erase start and end time (=0 because this can only occur if Daq-counter is at 00000000 which is an invalid word whose data shall not be used)
+# 3. Check valid Rising Edge
+# 	3.y.1. Check if there has been a starting signal
+#		3.y.1.n. Convert Signal to start time
+#		3.y.1.y. Check if there has been an end signal
+#		3.y.1.y.y. Discard
+#		3.y.1.y.n. Convert Signal to end time, calculate decay time and increment decay counter
+#		
+
+
+
+
 import argparse
 
 def argumente():
@@ -13,43 +30,41 @@ def argumente():
 
 opt=argumente()
 
+channel=0 #channel of the can at the Daq-card (0-3)
+channel_r=channel*2+1 #rising edge word of the channel 
+
 with open(opt.filename,"r") as datafile: #2014-06-03_15-29-27_RAW_1.01_ct
 	print("Successfully openend "+opt.filename)
-	counter=0
-	lasttime=0
-	linecounter=0
-	ch1=[]
+	event_counter=0
+	start_time=0
+	end_time=0
+	line_counter=0
 	dp=[]
 	for line in datafile:
 		#print line
-		linecounter=linecounter+1
-		if (linecounter%1000000==0):
-			print("%s lines already read" %linecounter) #is it alive???
+		line_counter=line_counter+1
+		if (line_counter%1000000==0):
+			print("%s lines already read" %line_counter) #is it alive???
 		line=line.strip()
 		splitter=line.split(' ')
-		if len(splitter)==16: #valid DAQ-Card-Word
-			time=int(splitter[0],16)
-			if time < lasttime:
-				counter=counter+1
-			time=time+counter*4294967295 #Counter restarts after 0xFFFFFFFF
-			time=time*0.00000004 #25 MHz
-			#if (int(splitter[1],16) & 0x80)!=0 : #valid trigger signal
-			if (int(splitter[1],16) & 0x20) !=0: #valid rising edge signal
-				temp=int(splitter[1],16) & 0x1F #save time bits
-				ch1.append(time+temp*0.00000000125)
-			lasttime=time
+		if len(splitter)==16: #valid DAQ-Card-Word? (1.)
+			time=int(splitter[0],16)*0.00000004 #25 MHz
+			if (int(splitter[1],16) & 0x80)!=0 : #valid trigger signal (2.)
+				event_counter=event_counter+1 #(2.y.1.)
+				start_time=0
+				end_time=0 #(2.y.2)
+			if (int(splitter[channel_r],16) & 0x20) !=0: #valid rising edge signal (3.)
+				if start_time == 0: #(3.y.1.)
+					temp=int(splitter[channel_r],16) & 0x1F #save time bits
+					start_time=time+temp*0.00000000125 #(3.y.1.n.)
+				elif end_time == 0: #(3.y.1.y.)
+					temp=int(splitter[channel_r],16) & 0x1F #save time bits
+					end_time=time+temp*0.00000000125 #(3.y.1.y.n.)					
+					dp.append(end_time-start_time)
+			
 
-print ("All %s lines read - checking now for double pulses" %linecounter)
+print ("All %s lines read - counted %s events and %s doublepulses" %(line_counter,event_counter,len(dp)))
 
-starttime=ch1[0]
-for i in range(1,len(ch1)):
-	endtime=ch1[i]
-	if ( (endtime-starttime) < 0.000010): #count every two signals with delta t < 10 micro seconds as double pulse
-		print(str(endtime)+"-"+str(starttime)+"="+str(endtime-starttime))
-		dp.append(endtime-starttime)
-	starttime=endtime
-
-print("Counted %s doublepulses" %len(dp) ) 
 filename="Timestamps_"+opt.filename
 with open(filename,"a") as writefile:
 	writefile.write("#Timestamps from file: "+opt.filename+"\n")
--- plot_threshold.py
+++ plot_threshold.py
@@ -2,61 +2,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.axes as ax
+import matplotlib as mpl
 import os
+import argparse
+
+def argumente():
+	"""Diese Funktion liest Argumente von der Kommandozeile ein."""
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('-f', '--filename', type=str, default=" ",
+	help="Name of the file to analyze.")
+	parser.add_argument('-x', '--xaxe', type=int, nargs="*", default=[0, 700],
+    help="Start- und Endpunkt der x-Achse (Schwellenspannung). Default ist %(default)s")
+
+			
+	return parser.parse_args()
+
+opt=argumente()
 
 #deefining lists for datapoints
 threshhold=[]
-rate=[]
-error_rate=[]
+rate_int=[]
+error_rate_int=[]
 
-#open all files
-for f in os.listdir(os.getcwd()):
-	with open(f,"r") as datafile:
-		counter=0
-		for line in datafile:
-			counter=counter+1
-			if counter == 2 : #check if datafile is from threshold measurement
-				if line.strip() != "#Schwelle\tMesszeit\tEreignisse":
-					break
-			if counter > 2: #read thresholds and calculate rate and the error of the rate
-				splitter=(line.strip()).split()
-				threshhold.append(float(splitter[0]))
-				rate.append(float(splitter[2])/float(splitter[1]))
-				error_rate.append(np.sqrt(float(splitter[2]))/float(splitter[1]))
-
-#convert arrays into numpy-arrays
-np_threshhold=np.array(threshhold)
-np_rate_int=np.array(rate)
-np_error_rate_int=np.array(error_rate)
+#open file
+with open(opt.filename,"r") as datafile:
+	counter=0
+	for line in datafile:
+		counter=counter+1
+		if counter == 2 : #check if datafile is from threshold measurement
+			if line.strip() != "#Schwelle\tMesszeit\tEreignisse":
+				break
+		if counter > 2: #read thresholds and calculate rate and the error of the rate
+			splitter=(line.strip()).split()
+			threshhold.append(float(splitter[0]))
+			rate_int.append(float(splitter[2])/float(splitter[1]))
+			error_rate_int.append(np.sqrt(float(splitter[2]))/float(splitter[1]))
 
 #sort arrays for further calculating from lowest to highest threshold
-index=np.lexsort((np_rate_int,np_threshhold))
-np_threshhold=np.array([np_threshhold[n] for n in index])
-np_rate_int=np.array([np_rate_int[n] for n in index])
-np_error_rate_int=np.array([np_error_rate_int[n] for n in index])
+index=np.lexsort((rate_int,threshhold))
+threshhold=[threshhold[n] for n in index]
+rate_int=[rate_int[n] for n in index]
+error_rate_int=[error_rate_int[n] for n in index]
 #print np_threshhold
 #print np_rate_int
 
 #differetiating rates...
-np_rate_diff=np.diff(np_rate_int)*(-1)
+rate_diff=[(-1)*(rate_int[n+1]-rate_int[n]) for n in range(len(threshhold)-1)]
 
 #calculating mean points between thresholds for plotting the differentiated rate
 threshhold_diff=[]
-for n in range(len(np_threshhold)-1):
-	threshhold_diff.append(np_threshhold[n]+(np_threshhold[n+1]-np_threshhold[n])/2)
-np_threshhold_diff=np.array(threshhold_diff)
+for n in range(len(threshhold)-1):
+	threshhold_diff.append(threshhold[n]+(threshhold[n+1]-threshhold[n])/2)
+
 
 #calculating error for rate_diff
 error_rate_diff=[]
-for n in range(len(np_threshhold)-1):
-	error_rate_diff.append(np.sqrt(np_error_rate_int[n+1]**2+np_error_rate_int[n]**2))
-np_error_rate_diff=np.array(error_rate_diff)
+for n in range(len(threshhold)-1):
+	error_rate_diff.append(np.sqrt(error_rate_int[n+1]**2+error_rate_int[n]**2))
+
 
 #plt.semilogy([np_threshhold,np_threshhold_diff],[np_rate_int,np_rate_diff],'x',basey=np.exp(1))
 plt.subplot(111,yscale="log") 
-plt.errorbar(np_threshhold,np_rate_int,yerr=np_error_rate_int,ls='None',marker='x',c="blue")
-plt.errorbar(np_threshhold_diff,np_rate_diff,yerr=np_error_rate_diff,ls='None',marker='x',c="red")
-plt.xlabel("Schwelle in mV")
-plt.ylabel("Logarithmische Auftragung der Rate in 1/s")
-plt.xlim(50,1050)
+plt.errorbar(threshhold,rate_int,yerr=error_rate_int,ls='None',marker='x',c="blue")
+plt.errorbar(threshhold_diff,rate_diff,yerr=error_rate_diff,ls='None',marker='x',c="red")
+plt.text(opt.xaxe[0]+(opt.xaxe[1]-opt.xaxe[0])/20.,0.001,r'Integrales Pulshoehenspektrum',fontsize=20,color='b')
+plt.text(opt.xaxe[0]+(opt.xaxe[1]-opt.xaxe[0])/20.,0.0001,r'Differentielles Pulshoehenspektrum',fontsize=20,color='r')
+plt.xlabel("Schwelle in mV",fontsize=19)
+font = {'family' : 'normal', 'weight' : 'bold','size': 19}
+mpl.rc('font', **font)#plt.ylabel("Logarithmische Auftragung der Rate in 1/s")
+plt.xlim(opt.xaxe[0],opt.xaxe[1])
 plt.show()
